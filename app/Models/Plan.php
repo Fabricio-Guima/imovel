db738ecdaa78366d5011bdd1869a525890b1b199
Fabricio-Guima <fsgkof@gmail.com>
2022-06-06 22:16:53 -0300

    #UuidTrait

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\UuidTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    use HasFactory;
+    use HasFactory, UuidTrait;
+
+    public $incrementing = false;
+    protected $keyType = 'uuid';
 }
